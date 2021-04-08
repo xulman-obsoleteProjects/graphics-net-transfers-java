b636013377ab837ab9dfe9c87e91cb85f7fd2419
Jan Kožusznik <jan@kozusznik.cz>
2021-04-08 14:13:38 +0200

    Empty implementation

---
+++ src/main/java/cz/it4i/ulman/graphics_net_transfers_java/server/SimViewProtocolMessagesProcessor.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * IT4Innovations - National Supercomputing Center
+ * Copyright (c) 2017 - 2021 All Right Reserved, https://www.it4i.cz
+ *
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE', which is part of this project.
+ ******************************************************************************/
+package cz.it4i.ulman.graphics_net_transfers_java.server;
+
+
+public class SimViewProtocolMessagesProcessor implements MessagesProcessor {
+
+	@Override
+	public void processMsg(String msg) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
